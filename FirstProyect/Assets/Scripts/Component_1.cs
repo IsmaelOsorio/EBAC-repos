7ad01feeab53b04f816d0d0e0862cfcba04d7580
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-07-01 17:37:16 -0600

    feat: Agregando objeto plúblico
    Se agrega el objeto myObject en el Component_1 para que en el Component_2 se pueda utilizar llamando lo. Pero es un error de Carrera porque el método  Awake en Component_2 se realiza primero antes de que el Component_1 se inicialice la variable.

--- FirstProyect/Assets/Scripts/Component_1.cs
+++ FirstProyect/Assets/Scripts/Component_1.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Component_1 : MonoBehaviour
 {
+    public static GameObject myObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myObject = this.gameObject;
     }
 
     // Update is called once per frame
--- FirstProyect/Assets/Scripts/Component_2.cs
+++ FirstProyect/Assets/Scripts/Component_2.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Component_2 : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log(Component_1.myObject.name);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
