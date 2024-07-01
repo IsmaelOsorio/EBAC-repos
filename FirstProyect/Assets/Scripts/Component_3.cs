3b74044d34d1b2d53cc3bb53ac0a44f50b8345b6
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-07-01 17:51:02 -0600

    feat: Cambiando el nombre del objeto
    En esta ocasión también puede suceder una Condición de Carrera, donde nosotros queremos que se ejecute primero es el Component_3 cambiando el nombre y después el Component_2 imprimiendo del nombre, pero puede que pase al revés.

--- FirstProyect/Assets/Scripts/Component_3.cs
+++ FirstProyect/Assets/Scripts/Component_3.cs
@@ -7,7 +7,7 @@ public class Component3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Component_1.myObject.name = "NewNameFromComponent_3";
     }
 
     // Update is called once per frame
