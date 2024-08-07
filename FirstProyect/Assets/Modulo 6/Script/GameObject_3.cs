ba4ee0675e0ca11c08767d9ab83b67e0b3968a6c
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-06 20:19:04 -0600

    feat: Se agregó funcionalidad de cambiar de color
    En la función FixedUpdate se cambia el color de la cápsula dando 3 valores random

--- FirstProyect/Assets/Modulo 6/Script/GameObject_3.cs
+++ FirstProyect/Assets/Modulo 6/Script/GameObject_3.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GameObject_3 : MonoBehaviour
 {
+    public GameObject PrefabCapsule;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Color colorObject = new Color(Random.value, Random.value, Random.value);
+        PrefabCapsule.GetComponent<MeshRenderer>().material.color = colorObject;
     }
 }
