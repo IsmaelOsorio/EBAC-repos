952a98c53d02722aac0cd6ed27ecf5c743a0028a
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-06 20:16:14 -0600

    feat: Se agregó funcionalidad de cambiar de color
    En la función Update se cambia el color de la esfera dando 3 valores random

--- FirstProyect/Assets/Modulo 6/Script/GameObject_2.cs
+++ FirstProyect/Assets/Modulo 6/Script/GameObject_2.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GameObject_2 : MonoBehaviour
 {
+    public GameObject PrefabSphere;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Color colorObject = new Color(Random.value, Random.value, Random.value);
+        PrefabSphere.GetComponent<MeshRenderer>().material.color = colorObject;
     }
 }
