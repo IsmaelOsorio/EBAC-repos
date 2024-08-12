d71158c2e8557d8e3932de8e5f222a8c974271f1
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-12 15:27:05 -0600

    feat: Se agrego funcionalidad de color a la Esfera
    En la función de Update se cambia el color de la Esfera, y como funciona en cada frame entonces cambia el color muy rápido

--- FirstProyect/Assets/Modulo 6/Script/Script_Esfera.cs
+++ FirstProyect/Assets/Modulo 6/Script/Script_Esfera.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Script_Esfera : MonoBehaviour
 {
+    public GameObject Prefab_Esfera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Color c = new Color(Random.value, Random.value, Random.value);
+        Prefab_Esfera.GetComponent<MeshRenderer>().material.color = c;
     }
 }
