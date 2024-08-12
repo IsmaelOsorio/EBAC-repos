4071744ca0c38376f951a832e59fcd9009cbe8db
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-12 15:44:47 -0600

    feat: Se agregó funcionalidad de crear Cubos
    En la función Awake se crean cubos, y como se ejecuta una sola vez entonces solo se crea uno

--- FirstProyect/Assets/Modulo 6/Script/Script_Crear_Cubos.cs
+++ FirstProyect/Assets/Modulo 6/Script/Script_Crear_Cubos.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Script_Crear_Cubos : MonoBehaviour
 {
+    public GameObject Prefab_Cubo;
+    public List<GameObject> listaCubos;
+    public int numCubos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        listaCubos = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Awake()
+    {
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefab_Cubo);
+        tempGameObject.name = "GameObject_Cubo_" + numCubos;
     }
 }
