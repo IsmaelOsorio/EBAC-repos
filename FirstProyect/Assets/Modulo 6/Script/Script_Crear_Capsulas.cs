1db3a72a3b07b72fddd07530c31b7fe8a8747892
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-12 17:05:39 -0600

    feat: Se agregó funcionalidad de crear Capsulas
    En las funciones OnDisable y OnEnable se crean Capsulas, y como se ejecuta cada vez que activamos o desactivamos el objeto entonces se crearan dependiendo de estas acciones

--- FirstProyect/Assets/Modulo 6/Script/Script_Crear_Capsulas.cs
+++ FirstProyect/Assets/Modulo 6/Script/Script_Crear_Capsulas.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Script_Crear_Capsulas : MonoBehaviour
 {
+    public GameObject Prefab_Esferas;
+    public List<GameObject> listaCubos;
+    public int numCubos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        listaCubos = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnDisable()
+    {
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefab_Esferas);
+        tempGameObject.name = "GameObject_Cubo_" + numCubos;
+    }
+
+    private void OnEnable()
+    {
+        numCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(Prefab_Esferas);
+        tempGameObject.name = "GameObject_Cubo_" + numCubos;
     }
 }
