d2af5d422546a34602f6c62840f4b18a22f532de
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-21 14:45:46 -0600

    feat: Se agregó funcionalidad en el Script del Cubo
    Si la variable es true se cambia el Cubo al color blanco y si es falso se cambia a negro

--- FirstProyect/Assets/Modulo 7/Script/Script_Bool_Cubo.cs
+++ FirstProyect/Assets/Modulo 7/Script/Script_Bool_Cubo.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Script_Cubo : MonoBehaviour
 {
+    public GameObject Prefab_Cubo;
+    public bool Bool_Cubo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Color c;
+        Bool_Cubo = !Bool_Cubo;
+
+        if (Bool_Cubo) c = new Color(255, 255, 255);
+        else c = new Color(0, 0, 0);
+
+        Prefab_Cubo.GetComponent<MeshRenderer>().material.color = c;
     }
 }
