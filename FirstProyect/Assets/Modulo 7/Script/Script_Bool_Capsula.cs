d66f41cd67f39fdf786e01dad807f58585119ec7
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-21 15:00:23 -0600

    feat: Se agregó funcionalidad en el Script de la Capsula
    Compara los valores de las variables en el Cubo y Esfera y hace una comparación AND para cambiar los colores

--- FirstProyect/Assets/Modulo 7/Script/Script_Bool_Capsula.cs
+++ FirstProyect/Assets/Modulo 7/Script/Script_Bool_Capsula.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Script_Capsula : MonoBehaviour
 {
+    public GameObject Prefab_Capsula;
+
+    public Script_Cubo cubo;
+    public Script_Esfera esfera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Color c;
+
+        if (cubo.Bool_Cubo && esfera.Bool_Esfera) c = new Color(255, 255, 255);
+        else c = new Color(0, 0, 0);
+
+        Prefab_Capsula.GetComponent<MeshRenderer>().material.color = c;
     }
 }
