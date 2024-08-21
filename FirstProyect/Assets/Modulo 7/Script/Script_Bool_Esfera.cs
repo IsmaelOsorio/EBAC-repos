eba3771f7a2ad6857ae3ac150c140d765594549e
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-08-21 14:51:48 -0600

    feat: Se agregó funcionalidad en el Script de la Esfera
    Si la variable es true se cambia la Esfera al color blanco y si es falso se cambia a negro

--- FirstProyect/Assets/Modulo 7/Script/Script_Bool_Esfera.cs
+++ FirstProyect/Assets/Modulo 7/Script/Script_Bool_Esfera.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Script_Esfera : MonoBehaviour
 {
+    public GameObject Prefab_Esfera;
+    public bool Bool_Esfera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Color c;
+        Bool_Esfera = !Bool_Esfera;
+
+        if (Bool_Esfera) c = new Color(255, 255, 255);
+        else c = new Color(0, 0, 0);
+
+        Prefab_Esfera.GetComponent<MeshRenderer>().material.color = c;
     }
 }
