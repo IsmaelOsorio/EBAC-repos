62d8984e0ac3cfb1e39675c33c7a02c076d9b110
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-09-04 16:51:16 -0600

    feat: Se completó la segunda sentencia
    Crea dos variables flotantes, has una operación entre ellas y obtén el resultado en un tipo de dato entero que requiera un casteo explícito.

--- FirstProyect/Assets/Modulo 7/Script/EjerciciosVariablesMod8.cs
+++ FirstProyect/Assets/Modulo 7/Script/EjerciciosVariablesMod8.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
 public class EjerciciosVariablesMod8 : MonoBehaviour
 {
     private int a   = 0;
     private float b = 2;
+
+    private float c = 3.932f;
+    private float d = 9.034f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"Operación entre flotantes: {(int)(c + d)}");
     }
 
     // Update is called once per frame
