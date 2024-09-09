12381f442dd9298e0ff724c8aabd747a3ab2fc2a
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-09-09 14:54:01 -0600

    feat: Se completó la tercera sentencia
    Multiplicación de matrices

--- FirstProyect/Assets/Modulo 7/Script/Ejercicios_de_Ciclos_y_arreglos.cs
+++ FirstProyect/Assets/Modulo 7/Script/Ejercicios_de_Ciclos_y_arreglos.cs
@@ -10,9 +10,14 @@ public class Ejercicios_de_Ciclos_y_arreglos : MonoBehaviour
 
     private string[] array_4;
 
+    private int[,] array_5;
+    private int[] array_6;
+    private int[] array_7;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Primera sentencia
         array_1 = new int[5];
         array_2 = new int[5];
         array_3 = new int[5];
@@ -32,6 +37,7 @@ public class Ejercicios_de_Ciclos_y_arreglos : MonoBehaviour
             Debug.Log($"Array 3: {array_3[i]}");
         }
 
+        // Segunda sentencia
         array_4 = new string[5] { "Aprende", "a vivir", "y sabrás", "morir", "bien" };
         string fraseCelebre = "";
 
@@ -41,6 +47,24 @@ public class Ejercicios_de_Ciclos_y_arreglos : MonoBehaviour
         }
 
         Debug.Log(fraseCelebre);
+
+        // Tercera sentencia
+        array_5 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+        array_6 = new int[3] { 1, 2, 3 };
+        array_7 = new int[array_5.GetLength(0)];
+
+        for (int i = 0; i < array_5.GetLength(0); i++)
+        {
+            for (int j = 0; j < array_6.Length; j++)
+            {
+                array_7[i] += (array_5[i, j] * array_6[j]);
+            }
+        }
+
+        foreach (int i in array_7)
+        {
+            Debug.Log($"Array 7: {i}");
+        }
     }
 
     // Update is called once per frame
