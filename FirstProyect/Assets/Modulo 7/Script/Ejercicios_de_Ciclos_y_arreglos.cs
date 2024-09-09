ec975239a7968c3965f313596d69e2ff97bcfe98
IsmaelOsorio <ismaelosorio28@gmail.com>
2024-09-09 13:29:41 -0600

    feat: Se completó la primera sentencia
    Crea dos arreglos unidimensionales de números enteros y llenalos usando un ciclo y la función Random.Range con números enteros, después crea un tercer arreglo y llénalo con la suma de los elementos de los primeros dos

--- FirstProyect/Assets/Modulo 7/Script/Ejercicios_de_Ciclos_y_arreglos.cs
+++ FirstProyect/Assets/Modulo 7/Script/Ejercicios_de_Ciclos_y_arreglos.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Ejercicios_de_Ciclos_y_arreglos : MonoBehaviour
 {
+    private int[] array_1;
+    private int[] array_2;
+    private int[] array_3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        array_1 = new int[5];
+        array_2 = new int[5];
+        array_3 = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            array_1[i] = Random.Range(0, 10);
+            array_2[i] = Random.Range(10, 20);
+
+            Debug.Log($"Array 1: {array_1[i]} \nArray 2: {array_2[i]}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            array_3[i] = array_1[i] + array_2[i];
+
+            Debug.Log($"Array 3: {array_3[i]}");
+        }
     }
 
     // Update is called once per frame
